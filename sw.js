b6b7cba8b4b221f95faa2ce26f6217eb54c87c34
KAMAL SANKHLA <44642421+ultra-precise@users.noreply.github.com>
2018-11-02 10:37:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -42,7 +42,10 @@ self.addEventListener('fetch', function(event) {
 });
 */
 
-
+self.addEventListener('activate', function (event)
+{
+    event.waitUntil(self.clients.claim());
+});
 
 
 self.addEventListener('fetch', event => {
