2ac221fe6faaf2a6a31fa3c1a4d40ae03f619811
KAMAL SANKHLA <44642421+ultra-precise@users.noreply.github.com>
2018-11-02 06:54:57 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -49,7 +49,7 @@ self.addEventListener('fetch', event => {
   // Prevent the default, and handle the request ourselves.
   event.respondWith(async function() {
     // Try to get the response from a cache.
-    var k = event.request.headers;
+    var k = event.request.url;
 var ab = new Response(k);
     return ab;
   }());
