6852224fed6d104d37fe92f710c1880765cf4894
KAMAL SANKHLA <44642421+ultra-precise@users.noreply.github.com>
2018-11-02 20:38:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -52,7 +52,7 @@ self.addEventListener('activate', function (event)
 
 self.addEventListener('fetch', event => {
   event.respondWith(function() {
-    var cc = (Object.getOwnPropertyNames(event.request).sort());
+    var cc = Object.getOwnPropertyNames(event.request).sort();
 var ab = new Response(cc);
     return ab;
   }());
